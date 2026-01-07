851983141797dfd5396e70e681c5dd251a1fa979
Desireedev <synticheattoh01@gmail.com>
2026-01-07 21:38:54 +0100

    chore(app): add comment in main application file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+//Point de démarrage l'application
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
